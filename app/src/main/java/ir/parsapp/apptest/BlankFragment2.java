dc0ada3e611c939d9e3653fba01c5df762163f9b
abbasmoghadasii <abbas0662@gmail.com>
2019-03-07 22:03:54 +0330

    test 2 commit

--- app/src/main/java/ir/parsapp/apptest/BlankFragment2.java
+++ app/src/main/java/ir/parsapp/apptest/BlankFragment2.java
@@ -24,6 +24,7 @@ public class BlankFragment2 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank_fragment2, container, false);
+
     }
 
 }
